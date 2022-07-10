febbb745a4eb5fa979a68e414c714fe7ce2b7f4b
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-07-10 12:26:45 +0300

    made the code cleaner and added to readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # HR-management-system
 
+## tsak 9
 added app.js and rendered employees names with their salaries on the home page
+
+## task 10
+created departments divs to sort employees by thier department
+used DOM to create the main body of my HTML
+created a div for the employee card and appended the picture and the info to it, and did some styling
+did some styling on the nav bars
--- js/app.js
+++ js/app.js
@@ -8,115 +8,91 @@ function Employee (ID, fullName, department, level){
     this.level = level;
     employees.push(this)
 }
-let employeesLevels = ["junior", "mid-senior", "senior"]
+let employeesLevels = ["junior", "mid-senior", "senior"];
 
 // calculate the salray
 Employee.prototype.salary= function () {
-    for (let i=0; i<employeesLevels.length; i++)
+    for (let i=0; i<employeesLevels.length; i++){
          if (this.level.toLowerCase () === employeesLevels[i]){
-            return this.salary = Math.random()*500+(500*(i+1)+1)}}
+            return this.salary = Math.random()*500+(500*(i+1)+1);
+        }
+    }
+}
 
 // calculate the salary after the tax
 Employee.prototype.netSalary= function () { this.salary();
-        return this.netSalary=  Math.floor(this.salary- this.salary*0.075)}
+        return this.netSalary=  Math.floor(this.salary- this.salary*0.075);
+}
 
 // save employees info in one array
-        let employeesFinalInfo =[];
-        Employee.prototype.printInfo = function () {
-            employeesFinalInfo.push (`<br><br>Name: ${this["Full Name"]}<br>ID: ${this["Employee ID"]}<br>Department: ${this.Department}<br>Level: ${this.level}<br>Salary: ${this.netSalary()} JD`)
-            }
-        
-        const ghaziSamer = new Employee (1000, "Ghazi Samer", "Administration", "Senior");
-        const lanaAli = new Employee (1001, "Lana Ali", "Finance", "Senior");
-        const tamaraAyoub = new Employee (1002, "Tamara Ayoub", "Marketing", "Senior");
-        const safiWalid = new Employee (1003, "Safi Walid", "Administration", "Mid-Senior");
-        const omarZaid = new Employee (1004, "Omar Zaid", "Development", "Senior");
-        const ranaSaleh = new Employee (1005, "Rana Saleh", "Development", "Junior");
-        const hadiAhmad = new Employee (1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+let employeesFinalInfo =[];
+Employee.prototype.printInfo = function () {
+        employeesFinalInfo.push (`<br><br>Name: ${this["Full Name"]}<br>ID: ${this["Employee ID"]}<br>Department: ${this.Department}<br>Level: ${this.level}<br>Salary: ${this.netSalary()} JD`);
+}
+
+//add your employees here
+const ghaziSamer = new Employee (1000, "Ghazi Samer", "Administration", "Senior");
+const lanaAli = new Employee (1001, "Lana Ali", "Finance", "Senior");
+const tamaraAyoub = new Employee (1002, "Tamara Ayoub", "Marketing", "Senior");
+const safiWalid = new Employee (1003, "Safi Walid", "Administration", "Mid-Senior");
+const omarZaid = new Employee (1004, "Omar Zaid", "Development", "Senior");
+const ranaSaleh = new Employee (1005, "Rana Saleh", "Development", "Junior");
+const hadiAhmad = new Employee (1006, "Hadi Ahmad", "Finance", "Mid-Senior");
         
         // trigger the info and fill the array
-        function printEmployeesInfo () {
-            for (let i=0; i<employees.length;i++){
-                employees[i].printInfo()}
-            }
-            printEmployeesInfo()
-            let imgSrc = ["./assets/Ghazi.jpg","./assets/Lana.jpg","./assets/Tamara.jpg","./assets/Safi.jpg","./assets/Omar.jpg","./assets/Rana.jpg","./assets/Hadi.jpg"]
+function printEmployeesInfo () {
+    for (let i=0; i<employees.length;i++){
+            employees[i].printInfo();
+    }
+}
+printEmployeesInfo();
+let imgSrc = ["./assets/Ghazi.jpg","./assets/Lana.jpg","./assets/Tamara.jpg","./assets/Safi.jpg","./assets/Omar.jpg","./assets/Rana.jpg","./assets/Hadi.jpg"]
                     
-            const main = document.getElementById("main")
-            main.style.margin="0px"
-            main.style.padding="0px"
+// create departments divs to sort employees by thier department
+const main = document.getElementById("main");
+let companyDepartments = ["Administration", "Marketing", "Development", "Finance"];
+let companyDepartmentDivs = [];
 
-            const Administration = document.createElement("div")
-            Administration.classList.add("administration")
-            const Marketing = document.createElement("div")
-            Marketing.classList.add("marketing")
-            const Development = document.createElement("div")
-            Development.classList.add("development")
-            const Finance = document.createElement("div")
-            Finance.classList.add("finance")
-           
-            main.append(Administration, Marketing, Development, Finance);
-            let companyDepartment = ["Administration", "Marketing", "Development", "Finance"];
-            let companyDepartmentDivs = [Administration, Marketing, Development, Finance];
+function departmentsSectionsCreater (){
+    for (let i=0; i<companyDepartments.length;i++){
+        let depart = document.createElement("div");
+        companyDepartmentDivs.push(depart);
+        let header = document.createElement("h2");
+        header.textContent = `${companyDepartments[i]} Department`;
+        main.append(depart);
+        depart.classList.add(`${companyDepartments[i]}`.toLowerCase());
+        depart.append(header);
+    }
+}
+departmentsSectionsCreater ();
 
-
-            const headerAdministration = document.createElement("h1");
-            const headerMarketing = document.createElement("h1");
-            const headerDevelopment = document.createElement("h1");
-            const headerFinance = document.createElement("h1");
-            let headers = [headerAdministration, headerMarketing, headerDevelopment, headerFinance]
-             // append the headers to the departmentsDivs
-            for (let i=0; i<companyDepartmentDivs.length;i++){
-                companyDepartmentDivs[i].append(headers[i])
-            }
-
-            headerAdministration.textContent = "Administration Department"
-            headerMarketing.textContent = "Marketing Department"
-            headerDevelopment.textContent = "Development Department"
-            headerFinance.textContent = "Finance Department"
-
-
-
-
-            // header.style.marginTop = "20%"
-            // header.style.textAlign = "center"
-
-            // use DOM to create the main body of our HTML
-            function renderMain() {
-            for (let j=0; j<imgSrc.length;j++){
-                // creat div for each department
-                for (let i=0; i<companyDepartment.length;i++){
-                if (employeesFinalInfo[j].includes(companyDepartment[i])){
+// use DOM to create the main body of our HTML
+function renderMain() {
+    for (let j=0; j<imgSrc.length;j++){
+        // creat div for each department
+        for (let i=0; i<companyDepartments.length;i++){
+            if (employeesFinalInfo[j].includes(companyDepartments[i])){
                 // create a div for each employee 
-                const employeeDiv = document.createElement("div")
-                employeeDiv.style.marginTop ="3%"
-                employeeDiv.style.marginBottom ="4%"
-                employeeDiv.style.display = "flex"
-                employeeDiv.style.textAlign= "center"
-                companyDepartmentDivs[i].append(employeeDiv)
-                //create a div for the mployee card and append the picture<img> and the info<p> to it, and do some styling
-                const employeeInfoParagraph = document.createElement("div")
+                const employeeDiv = document.createElement("div");
+                employeeDiv.classList.add("employeeDiv");
+                companyDepartmentDivs[i].append(employeeDiv);
+                //create a div for the employee card and append the picture<img> and the info<p> to it, and do some styling
+                const employeeInfoParagraph = document.createElement("div");
                 employeeInfoParagraph.classList.add("employeeCard");
-                employeeInfoParagraph.style.margin = "20px 35% 20px 35%"
-                // employeeInfoParagraph.style.paddingLeft = "5px"
-                employeeInfoParagraph.style.paddingTop = "50px"
-                employeeInfoParagraph.style.borderRadius = "20px"
-            const employeeImg = document.createElement("img")
-            employeeImg.classList.add("employeePic");
-            employeeDiv.append(employeeInfoParagraph)
-            employeeImg.src=imgSrc[j]
-            employeeImg.style.width = "250px";
-            employeeImg.style.borderRadius = "150px"
-            employeeInfoParagraph.append(employeeImg)
-            const info = document.createElement("p")
-            info.style.padding ="50px"
-            info.classList.add("emplyeeInfo");
-            info.innerHTML = employeesFinalInfo[j]
-           employeeInfoParagraph.append(info)
-            }
+                const employeeImg = document.createElement("img");
+                employeeImg.classList.add("employeePic");
+                employeeDiv.append(employeeInfoParagraph);
+                employeeImg.src=imgSrc[j];
+                employeeInfoParagraph.append(employeeImg);
+                const info = document.createElement("p");
+                info.style.padding ="50px";
+                info.classList.add("emplyeeInfo");
+                info.innerHTML = employeesFinalInfo[j];
+                employeeInfoParagraph.append(info);
             }
         }
     }
+}
 
-            renderMain()
+renderMain();
 
