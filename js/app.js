edd0efadc7a42ec3e3a36981b6c69f4591189df2
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-07-13 12:40:18 +0300

    added the imgSrc to the constructor

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 "use strict";
 // create a constructor 
 const employees =[];
-function Employee (ID, fullName, department, level){
+function Employee (ID, fullName, department, level, imgSrc){
     this["Employee ID"] = ID;
     this["Full Name"] = fullName;
     this.Department = department;
     this.level = level;
+    this.picture = imgSrc;
     employees.push(this)
 }
 let employeesLevels = ["junior", "mid-senior", "senior"];
@@ -31,14 +32,14 @@ Employee.prototype.printInfo = function () {
 }
 
 //add your employees here
-const ghaziSamer = new Employee (1000, "Ghazi Samer", "Administration", "Senior");
-const lanaAli = new Employee (1001, "Lana Ali", "Finance", "Senior");
-const tamaraAyoub = new Employee (1002, "Tamara Ayoub", "Marketing", "Senior");
-const safiWalid = new Employee (1003, "Safi Walid", "Administration", "Mid-Senior");
-const omarZaid = new Employee (1004, "Omar Zaid", "Development", "Senior");
-const ranaSaleh = new Employee (1005, "Rana Saleh", "Development", "Junior");
-const hadiAhmad = new Employee (1006, "Hadi Ahmad", "Finance", "Mid-Senior");
-        
+const ghaziSamer = new Employee (1000, "Ghazi Samer", "Administration", "Senior","./assets/Ghazi.jpg");
+const lanaAli = new Employee (1001, "Lana Ali", "Finance", "Senior","./assets/Lana.jpg");
+const tamaraAyoub = new Employee (1002, "Tamara Ayoub", "Marketing", "Senior","./assets/Tamara.jpg");
+const safiWalid = new Employee (1003, "Safi Walid", "Administration", "Mid-Senior","./assets/Safi.jpg");
+const omarZaid = new Employee (1004, "Omar Zaid", "Development", "Senior","./assets/Omar.jpg");
+const ranaSaleh = new Employee (1005, "Rana Saleh", "Development", "Junior","./assets/Rana.jpg");
+const hadiAhmad = new Employee (1006, "Hadi Ahmad", "Finance", "Mid-Senior","./assets/Hadi.jpg");
+
         // trigger the info and fill the array
 function printEmployeesInfo () {
     for (let i=0; i<employees.length;i++){
@@ -46,7 +47,6 @@ function printEmployeesInfo () {
     }
 }
 printEmployeesInfo();
-let imgSrc = ["./assets/Ghazi.jpg","./assets/Lana.jpg","./assets/Tamara.jpg","./assets/Safi.jpg","./assets/Omar.jpg","./assets/Rana.jpg","./assets/Hadi.jpg"]
                     
 // create departments divs to sort employees by their department
 const main = document.getElementById("main");
@@ -68,7 +68,7 @@ departmentsSectionsCreater ();
 
 // use DOM to create the main body of our HTML
 function renderMain() {
-    for (let j=0; j<imgSrc.length;j++){
+    for (let j=0; j<employees.length;j++){
         // creat div for each department
         for (let i=0; i<companyDepartments.length;i++){
             if (employeesFinalInfo[j].includes(companyDepartments[i])){
@@ -83,7 +83,7 @@ function renderMain() {
                 employeeImg.setAttribute("alt",`${employees[j]["Full Name"]}`.toLowerCase());
                 employeeImg.classList.add("employeePic");
                 employeeDiv.append(employeeInfoParagraph);
-                employeeImg.src=imgSrc[j];
+                employeeImg.src=employees[j].picture;
                 employeeInfoParagraph.append(employeeImg);
                 const info = document.createElement("p");
                 info.style.padding ="50px";
